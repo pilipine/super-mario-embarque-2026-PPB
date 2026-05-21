f7fece6dbd839c96d20c5e8032dd7113d8d3d83b
pilipine <philippinepelletratdeborde@gmail.com>
2026-05-21 10:37:53 +0200

    ajout des commentaires

--- code.cpp
+++ code.cpp
@@ -21,12 +21,12 @@ TFT_eSPI tft = TFT_eSPI();
 #define GRID_LINE    0x2124 
 
 // --- Variables de Jeu ---
-enum Page { PRINCIPAL, NIVEAUX, PARAMETRES, COULEUR, JEU_NIV1, JEU_NIV2, JEU_NIV3 };
-Page pageActuelle = PRINCIPAL;
+enum Page { PRINCIPAL, NIVEAUX, PARAMETRES, COULEUR, JEU_NIV1, JEU_NIV2, JEU_NIV3 };  //Crée une liste de mots-clés numérotés pour identifier facilement chaque écran du jeu.
+Page pageActuelle = PRINCIPAL; //Crée la variable qui mémorise l'écran actuellement affiché et démarre le jeu sur l'accueil.
 
 uint16_t couleurMarioActive = MY_RED; 
 int indexCurseurCouleur = 0;
-uint16_t palette[] = {0xF844, 0xFFE0, 0xF81F, 0x001F, 0x0400, 0x780F, 0xD6BA};
+uint16_t palette[] = {0xF844, 0xFFE0, 0xF81F, 0x001F, 0x0400, 0x780F, 0xD6BA}; // palette de couleur de mes marios
 
 int marioX, marioY;
 const int cibleX = 15, cibleY = 7;
@@ -77,30 +77,30 @@ void handleMenuCouleurLogic() {
 }
 
 // --- DEPLACEMENT DES ENNEMIS ---
-void deplacerEnnemis() {
-  for (int i = 0; i < nbEnnemisActuels; i++) {
-    int dir = random(4); 
-    int nx = ennemiX[i], ny = ennemiY[i];
-    if (dir == 0) nx++; 
-    else if (dir == 1) nx--; 
-    else if (dir == 2) ny++; 
-    else if (dir == 3) ny--;
-    
-    if (nx >= 0 && nx <= 15 && ny >= 0 && ny <= 7 && grille[nx][ny] == 0) { 
-      ennemiX[i] = nx; 
-      ennemiY[i] = ny; 
+void deplacerEnnemis() { //Déclare la fonction qui va gérer les mouvements automatiques des adversaires.
+  for (int i = 0; i < nbEnnemisActuels; i++) { //Lance une boucle pour faire bouger chaque ennemi .
+    int dir = random(4); // : Choisit un nombre au hasard entre 0 et 3 pour décider d'une direction au hasard (haut, bas, gauche, droite).
+    int nx = ennemiX[i], ny = ennemiY[i]; //  Copie la position actuelle de l'ennemi dans des variables temporaires pour calculer son futur déplacement.
+    if (dir == 0) nx++; //  Modifie la coordonnée horizontale vers la droite si le hasard a choisi la direction zéro.
+    else if (dir == 1) nx--; // Modifie la coordonnée horizontale vers la gauche si le hasard a choisi la direction un.
+    else if (dir == 2) ny++; // Modifie la coordonnée verticale vers le bas si le hasard a choisi la direction deux.
+    else if (dir == 3) ny--; // Modifie la coordonnée verticale vers le haut si le hasard a choisi la direction trois.
+
+    if (nx >= 0 && nx <= 15 && ny >= 0 && ny <= 7 && grille[nx][ny] == 0) { // Vérifie si la case visée par l'ennemi reste dans l'écran et ne contient aucun mur.
+      ennemiX[i] = nx; // Valide le déplacement et met à jour la position horizontale définitive de l'ennemi.
+      ennemiY[i] = ny; // Valide le déplacement et met à jour la position verticale définitive de l'ennemi.
     }
   }
 }
 
-void finDePartie(String txt, uint16_t color) {
+void finDePartie(String txt, uint16_t color) { // fin de la partie et message de fin 
   tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(color);
+  tft.setTextColor(color); // Configure la couleur du texte (vert pour gagné, rouge pour perdu).
   tft.setTextSize(4);
-  tft.setCursor(50, 100);
-  tft.print(txt);
+  tft.setCursor(50, 100); // Place le curseur d'écriture aux coordonnées x=50 et y=100 pour centrer le message à l'écran.
+  tft.print(txt); // Écrit le texte de fin ("GAGNE !" ou "GAME OVER") sur l'écran.
   delay(3000);
-  changerDePage(NIVEAUX);
+  changerDePage(NIVEAUX); // Renvoie automatiquement le joueur vers l'écran de sélection des niveaux pour recommencer.
 }
 
 // --- VERIFICATION DES COLLISIONS ---
@@ -118,19 +118,19 @@ void verifierCollisions() {
 }
 
 // --- LOGIQUE DE JEU ---
-void handleGameLogic() {
+void handleGameLogic() { // Déclare la fonction qui gère les actions du joueur pendant une partie.
   int nx = marioX, ny = marioY;
-  bool moved = false;
+  bool moved = false; //Crée une variable de contrôle pour savoir si le joueur a appuyé sur un bouton de direction.
 
-  if (digitalRead(BTN_ORANGE) == HIGH) { nx--; moved = true; }
-  else if (digitalRead(BTN_JAUNE) == HIGH) { nx++; moved = true; }
-  else if (digitalRead(BTN_BLEU) == HIGH) { ny--; moved = true; }
-  else if (digitalRead(BTN_VERT) == HIGH) { ny++; moved = true; }
+  if (digitalRead(BTN_ORANGE) == HIGH) { nx--; moved = true; } //  gauche
+  else if (digitalRead(BTN_JAUNE) == HIGH) { nx++; moved = true; } // droite
+  else if (digitalRead(BTN_BLEU) == HIGH) { ny--; moved = true; } // haut
+  else if (digitalRead(BTN_VERT) == HIGH) { ny++; moved = true; } // bas
 
-  if (moved) {
-    if (nx >= 0 && nx <= 15 && ny >= 0 && ny <= 7 && grille[nx][ny] == 0) {
-      marioX = nx; marioY = ny;
-      if (nbEnnemisActuels > 0) deplacerEnnemis();
+  if (moved) { // si bouton appuyer alors bouger
+    if (nx >= 0 && nx <= 15 && ny >= 0 && ny <= 7 && grille[nx][ny] == 0) { //Vérifie que la nouvelle case visée ne sort pas de l'écran et qu'elle ne contient pas un mur.
+      marioX = nx; marioY = ny; // Valide définitivement le déplacement en appliquant les nouvelles coordonnées à Mario.
+      if (nbEnnemisActuels > 0) deplacerEnnemis(); // : Fait bouger les ennemis d'une case juste après le mouvement de Mario si le niveau en contient
       drawGrilleJeu();
       verifierCollisions();
     }
@@ -139,7 +139,7 @@ void handleGameLogic() {
 }
 
 // --- AFFICHAGE ---
-void drawGrilleJeu() {
+void drawGrilleJeu() { // (L'écran de partie)
   tft.fillScreen(0x0841);
   tft.drawRoundRect(10, 10, 300, 40, 5, MY_BLUE);
   tft.setTextColor(TFT_WHITE); tft.setTextSize(2);
@@ -162,7 +162,7 @@ void drawGrilleJeu() {
   tft.fillRect(gridX + (cibleX*cellW)+2, gridY + (cibleY*cellH)+2, cellW-4, cellH-4, 0x07FF);
 }
 
-void drawCommonUI() {
+void drawCommonUI() { // (L'habillage des menus)
   tft.fillRect(0, 200, 320, 40, MY_GREY);
   tft.fillRect(45, 15, 230, 65, MY_RED);
   tft.setTextColor(TFT_WHITE); tft.setTextSize(3);
@@ -170,13 +170,13 @@ void drawCommonUI() {
   tft.setCursor(60, 45); tft.print("mario bros");
 }
 
-void drawButton(int y, uint16_t color, String label) {
+void drawButton(int y, uint16_t color, String label) { // (Le créateur de boutons)
   tft.fillRoundRect(80, y, 160, 30, 15, color);
   tft.setTextColor(TFT_WHITE); tft.setTextSize(2);
   tft.setCursor(160 - (label.length()*6), y + 8); tft.print(label);
 }
 
-void drawMenuCouleurs() {
+void drawMenuCouleurs() { // (Selecteur de couleur Mario)
   tft.fillScreen(MY_SKY); drawCommonUI();
   tft.setTextColor(TFT_WHITE); tft.setTextSize(2);
   tft.setCursor(65, 90); tft.print("choisis ta couleur");
@@ -207,13 +207,13 @@ void initialiserJeu(Page niv) {
   else if (niv == JEU_NIV3) nbEnnemisActuels = 11;
   
   // Génération de la grille
-  for (int x = 0; x < 16; x++) {
-    for (int y = 0; y < 8; y++) {
+  for (int x = 0; x < 16; x++) { //Parcourt la grille du jeu colonne par colonne, de gauche à droite.
+    for (int y = 0; y < 8; y++) { // Parcourt chaque ligne de la grille, de haut en bas, pour analyser chaque case.
       // Zone de sécurité 2x2 en haut à gauche pour éviter de bloquer Mario au départ
-      if (x <= 1 && y <= 1) {
-        grille[x][y] = 0; 
-      } else {
-        grille[x][y] = (random(100) < 15) ? 1 : 0;
+      if (x <= 1 && y <= 1) { //Vérifie si la case actuelle se trouve dans le carré de départ en haut à gauche.
+        grille[x][y] = 0; //Force la case à être vide pour que le joueur ne commence pas la partie bloqué dans un mur.
+      } else { // Indique au programme quoi faire pour toutes les autres cases situées en dehors de la zone de départ.
+        grille[x][y] = (random(100) < 15) ? 1 : 0; // Donne à la case 15% de risques de devenir un mur et 85% de chances de rester libre.
       }
     }
   }
@@ -221,10 +221,10 @@ void initialiserJeu(Page niv) {
   grille[cibleX][cibleY] = 0; // Sécurité pour la zone d'arrivée
   
   // Placement des ennemis
-  for (int i = 0; i < nbEnnemisActuels; i++) { 
-    ennemiX[i] = random(5, 15); 
-    ennemiY[i] = random(0, 7); 
-    grille[ennemiX[i]][ennemiY[i]] = 0; 
+  for (int i = 0; i < nbEnnemisActuels; i++) { // Lance une boucle pour positionner chaque ennemi l'un après l'autre,
+    ennemiX[i] = random(5, 15); // Choisit une colonne au hasard entre la 5ème et la 15ème pour placer l'ennemi sur la partie droite de l'écran
+    ennemiY[i] = random(0, 7); // Choisit une ligne au hasard entre la 0 (tout en haut) et la 7 (tout en bas) pour la hauteur de l'ennemi.
+    grille[ennemiX[i]][ennemiY[i]] = 0; //Force la case choisie à être vide (valeur 0) afin de supprimer un éventuel mur pour que l'ennemi ne commence pas la partie coincé.
   }
   
   changerDePage(niv);
@@ -264,8 +264,8 @@ void setup() {
   drawMenuPrincipal();
 }
 
-void loop() {
-  if (pageActuelle == COULEUR) handleMenuCouleurLogic();
-  else if (pageActuelle >= JEU_NIV1) handleGameLogic();
-  else handleMenuNavigation();
+void loop() { // Démarre la fonction principale d'Arduino qui s'exécute en continu et en boucle tant que le jeu est allumé.
+  if (pageActuelle == COULEUR) handleMenuCouleurLogic(); // Active les commandes de sélection des couleurs si le joueur se trouve sur cet écran spécifique.
+  else if (pageActuelle >= JEU_NIV1) handleGameLogic(); Active les règles de la partie (mouvements, collisions) si le joueur est en train de jouer à un niveau.
+  else handleMenuNavigation(); Gère les boutons pour naviguer dans les menus classiques si aucune partie n'est lancée.
 }
